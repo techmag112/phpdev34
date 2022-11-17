dc3dabf2a86d98bf4941fd34aeb1b66f3579e281
Oleg Khayrullin <oleg.khayrullin@gmail.com>
2022-11-17 20:50:47 +0300

    html practica 1-4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-const lines = 5;
-let result = '';
-    // Пишем решение вот тут
-    // Проверяется именно переменная result, формируйте строку в ней
-       for (let i = 0; i <= lines; i++) {
-            for (let j = 0; j < (lines - i); j++) {
-                result += " ";
-            }
-            for (let j = 0; j < (11 - ((lines - i)*2)); j++) {
-                result += "*";
-            }
-                if (i <= lines+1) result += "\n";
-            }
-        
-    console.log(result);
+"use sсtrict";
